2c1e8b762ae5ac12423f7430d4adb69efd09b8ad
Juan Pedro del Campo <pericodc@gmail.com>
2012-08-01 10:36:16 -0300

    Incio

---
+++ tests/application/controllers/GuestbookControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once 'PHPUnit/Framework/TestCase.php';
+
+class GuestbookControllerTest extends PHPUnit_Framework_TestCase
+{
+
+    public function setUp()
+    {
+        /* Setup Routine */
+    }
+
+    public function tearDown()
+    {
+        /* Tear Down Routine */
+    }
+
+
+    public function testAll()
+    {
+        /* Add your test code here */
+    }
+}
+
